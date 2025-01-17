fda481f038e7a73937dcaf58e177050dd1d2b246
Corentin Cousty <45534019+Kaldah@users.noreply.github.com>
2025-01-17 16:14:51 +0100

    Ajout de commentaires et petites modifs

--- UART_emission/compteur16.vhd
+++ UART_emission/compteur16.vhd
@@ -2,65 +2,77 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity Compteur16 is
-    Port (
-        reset : in std_logic;
-        RxD : in std_logic;
-        enable : in std_logic;
-        tmpclk : out std_logic;
-        tmprxd : out std_logic
-    );
+  port (
+    reset  : in std_logic;
+    RxD    : in std_logic;
+    enable : in std_logic;
+    tmpclk : out std_logic;
+    tmprxd : out std_logic
+  );
 end Compteur16;
 
 architecture Behavioral of Compteur16 is
-    signal count : integer range 0 to 15 := 0;
-
-    type t_etat is (repos, count8, count16);
-    signal etat : t_etat := repos;
-    signal counter_bits : integer range 0 to 10 := 0;
+  -- Compteur de fronts d'enable
+  signal count : integer range 0 to 15 := 0;
+  -- Compteur de bits envoyés
+  signal counter_bits : integer range 0 to 10 := 0;
+  type t_etat is (repos, count8, count16);
+  signal etat         : t_etat                := repos;
 
 begin
-    process(enable, reset)
-    begin
-        if reset = '0' then
-            count <= 0;
+  process (enable, reset)
+  begin
+    if reset = '0' then
+      count  <= 0;
+      tmpclk <= '0';
+      tmprxd <= '0';
+      etat   <= repos;
+      -- L'horloge est sur les fronts montant d'enable
+    elsif rising_edge(enable) then
+      case etat is
+        
+        -- Etat de repos, attente du bit de start
+        when repos =>
+          count  <= 0;
+          tmpclk <= '0';
+          tmprxd <= '0';
+          -- Bit de start reçu
+          if RxD = '0' then
+            etat <= count8;
+          end if;
+        
+        -- Attend 8 fronts d'enable
+        when count8 =>
+          count <= count + 1;
+          -- Envoie un signal tmpclk et transmet le bit
+          if count = 7 then
+            tmpclk <= '1';
+            tmprxd <= RxD;
+            count  <= 0;
+            etat   <= count16;
+          end if;
+        
+        --  Attend 8 fronts d'enable
+        when count16 =>
+          count <= count + 1;
+          -- Envoie un signal tmpclk et transmet le bit au 16e front
+          if count = 15 then
+            tmprxd <= RxD;
+            tmpclk <= '1';
+            count  <= 0;
+            -- Le 11e bit vient d'être envoyé
+            if counter_bits = 10 then
+              counter_bits <= 0;
+              etat         <= repos;
+            -- Il reste des bits à envoyer, on passe au suivant
+            else
+              counter_bits <= counter_bits + 1;
+            end if;
+          -- tmpclk reste à 0 pendant qu'on compte
+          else
             tmpclk <= '0';
-            tmprxd <= '0';
-            etat <= repos;
-        elsif rising_edge(enable) then
-            case etat is
-                when repos =>
-                    count <= 0;
-                    tmpclk <= '0';
-                    tmprxd <= '0';
-                    if RxD = '0' then
-                        etat <= count8;
-                    end if;
-                when count8 =>
-                    count <= count + 1;
-                    if count = 7 then
-                        tmpclk <= '1';
-                        tmprxd <= RxD;
-                        count <= 0;
-                        etat <= count16;
-                    end if;
-                when count16 =>
-                    count <= count + 1;
-                    
-                    if count = 15 then
-                        tmprxd <= RxD;
-                        tmpclk <= '1';
-                        count <= 0;
-                        -- 11 bits bits
-                        if counter_bits = 10 then
-                            counter_bits <= 0;
-                            etat <= repos;
-                        else
-                            counter_bits <= counter_bits + 1;
-                        end if;
-                    else
-                        tmpclk <= '0';
-                    end if;
-            end case;
-        end if;
-    end process;
+          end if;
+      end case;
+    end if;
+  end process;
 end Behavioral;
--- UART_emission/controle_reception.vhd
+++ UART_emission/controle_reception.vhd
@@ -16,44 +16,46 @@ entity ControleReception is
 end ControleReception;
 
 architecture Behavioral of ControleReception is
-    signal received_data : std_logic_vector(7 downto 0) := (others => '0');
+    -- Compteurs de bits envoyés après le bit de start
     signal bit_count : integer range 0 to 9 := 0;
+    -- Bit de parité calculé en réception
     signal bit_parite : std_logic := '0';
-
+    -- Les différents états
     type t_etat is (repos, reception, transfert);
     signal etat : t_etat := repos;
 begin
     process(clk, reset)
         variable tmpFErr : std_logic := '0';
-
     begin
         if reset= '0' then
-            received_data <= (others => '0');
+            data <= (others => '0');
             bit_count <= 0;
             Ferr <= '0';
             OErr <= '0';
             DRdy <= '0';
             etat <= repos;
         elsif rising_edge(clk) then
-            case etat is 
+            case etat is
                 when repos =>
                     Ferr <= '0';
                     OErr <= '0';
                     DRdy <= '0';
                     bit_count <= 0;
-
+                    -- On attend le bit de start
                     if tmpclk = '1' and tmprxd = '0' then
                         etat <= reception;
                     end if;
 
+                -- On réceptionne l'octet (8 bits), le bit de parité et le bit de stop
                 when reception =>
                     if tmpclk = '1' then
+                        -- On réceptionne le bit de stop
                         if bit_count = 9 then
                             -- On vérifie si le bit de stop est valide: tmprxd = 1
                             -- En cas d'erreur FErr = 1
                             tmpFErr := tmpFErr or (not tmprxd);
-                            
-                            if (tmpFErr = '0') then
+                            -- Le bit de stop est invalide, retour au repos et on remonte l'erreur
+                            if (tmpFErr = '1') then
                                 FErr <= tmpFErr;
                                 etat <= repos;
                             -- Le bit de stop est valide
@@ -61,23 +63,27 @@ begin
                                 DRdy <= '1';
                                 etat <= transfert;
                             end if;
+                        -- On réceptionne le bit de parité
                         elsif bit_count = 8 then
                             -- On vérifie si le bit de parité est valide
                             tmpFErr := not (bit_parite xor tmprxd);
                             bit_count <= bit_count + 1;
+                        -- Réception de l'octet
                         elsif bit_count < 8 then
                             -- On les reçoit de du 7e au 0e bit
-                            received_data(7 - bit_count) <= tmprxd;
+                            data(7 - bit_count) <= tmprxd;
                             bit_count <= bit_count + 1;
                             bit_parite <= bit_parite xor tmprxd;
                         end if;
                     end if;
+                
+                -- On transfert l'octet reçu
                 when transfert =>
                     DRdy <= '0';
+                    -- On vérifie si le processeur a lu l'octet
                     if read = '0'then
                         OErr <= '1';
                     end if;
-                    data <= received_data;
                     etat <= repos;
             end case;
         end if;
